3e15fdf2953d2f6261a93bbd517a145b964b3c42
Aimeos <aimeos@aimeos.org>
2020-01-07 10:13:38 +0100

    Use READ COMMITTED isolation level

--- src/aimeos-default.php
+++ src/aimeos-default.php
@@ -1,285 +1,285 @@
 <?php
 
-return array(
+return [
 
 	'apc_enabled' => false,
 	'apc_prefix' => 'slim:',
 	'uploaddir' => '/',
 
-	'page' => array(
-		'account-index' => array( 'account/profile', 'account/subscription', 'account/history', 'account/favorite', 'account/watch', 'basket/mini', 'catalog/session' ),
-		'basket-index' => array( 'basket/bulk', 'basket/standard', 'basket/related' ),
-		'catalog-count' => array( 'catalog/count' ),
-		'catalog-detail' => array( 'basket/mini', 'catalog/stage', 'catalog/detail', 'catalog/session' ),
-		'catalog-list' => array( 'basket/mini', 'catalog/filter', 'catalog/lists' ),
-		'catalog-stock' => array( 'catalog/stock' ),
-		'catalog-suggest' => array( 'catalog/suggest' ),
-		'catalog-tree' => array( 'basket/mini', 'catalog/filter', 'catalog/stage', 'catalog/lists' ),
-		'checkout-confirm' => array( 'checkout/confirm' ),
-		'checkout-index' => array( 'checkout/standard' ),
-		'checkout-update' => array( 'checkout/update' ),
-	),
+	'page' => [
+		'account-index' => [ 'account/profile', 'account/subscription', 'account/history', 'account/favorite', 'account/watch', 'basket/mini', 'catalog/session' ],
+		'basket-index' => [ 'basket/bulk', 'basket/standard', 'basket/related' ],
+		'catalog-count' => [ 'catalog/count' ],
+		'catalog-detail' => [ 'basket/mini', 'catalog/stage', 'catalog/detail', 'catalog/session' ],
+		'catalog-list' => [ 'basket/mini', 'catalog/filter', 'catalog/lists' ],
+		'catalog-stock' => [ 'catalog/stock' ],
+		'catalog-suggest' => [ 'catalog/suggest' ],
+		'catalog-tree' => [ 'basket/mini', 'catalog/filter', 'catalog/stage', 'catalog/lists' ],
+		'checkout-confirm' => [ 'checkout/confirm' ],
+		'checkout-index' => [ 'checkout/standard' ],
+		'checkout-update' => [ 'checkout/update' ],
+	],
 
-	'resource' => array(
-		'db' => array(
+	'resource' => [
+		'db' => [
 			'adapter' => 'mysql',
 			'host' => 'localhost',
 			'port' => '',
 			'database' => 'slim',
 			'username' => 'aimeos',
 			'password' => 'aimeos',
-			'stmt' => array( "SET SESSION sort_buffer_size=2097144; SET NAMES 'utf8mb4'; SET SESSION sql_mode='ANSI'" ),
+			'stmt' => [ "SET SESSION sort_buffer_size=2097144; SET NAMES 'utf8mb4'; SET SESSION sql_mode='ANSI'; SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED" ],
 			'opt-persistent' => 0,
 			'limit' => 3,
 			'defaultTableOptions' => [
 				'charset' => 'utf8mb4',
 				'collate' => 'utf8mb4_bin',
 			],
-		),
-		'fs' => array(
+		],
+		'fs' => [
 			'adapter' => 'Standard',
 			'basedir' => './',
 			'baseurl' => '/',
-		),
-		'fs-admin' => array(
+		],
+		'fs-admin' => [
 			'adapter' => 'Standard',
 			'basedir' => './uploads',
-		),
-		'fs-import' => array(
+		],
+		'fs-import' => [
 			'adapter' => 'Standard',
 			'basedir' => '../secure/import',
-		),
-		'fs-secure' => array(
+		],
+		'fs-secure' => [
 			'adapter' => 'Standard',
 			'basedir' => '../secure',
-		),
-		'mq' => array(
+		],
+		'mq' => [
 			'adapter' => 'Standard',
 			'db' => 'db',
-		),
-	),
+		],
+	],
 
-	'admin' => array(
-		'jqadm' => array(
-			'url' => array(
-				'copy' => array(
+	'admin' => [
+		'jqadm' => [
+			'url' => [
+				'copy' => [
 					'target' => 'aimeos_shop_jqadm_copy'
-				),
-				'create' => array(
+				],
+				'create' => [
 					'target' => 'aimeos_shop_jqadm_create'
-				),
-				'delete' => array(
+				],
+				'delete' => [
 					'target' => 'aimeos_shop_jqadm_delete'
-				),
-				'export' => array(
+				],
+				'export' => [
 					'target' => 'aimeos_shop_jqadm_export'
-				),
-				'get' => array(
+				],
+				'get' => [
 					'target' => 'aimeos_shop_jqadm_get'
-				),
-				'save' => array(
+				],
+				'save' => [
 					'target' => 'aimeos_shop_jqadm_save'
-				),
-				'search' => array(
+				],
+				'search' => [
 					'target' => 'aimeos_shop_jqadm_search'
-				),
-			)
-		),
-		'jsonadm' => array(
-			'url' => array(
+				],
+			]
+		],
+		'jsonadm' => [
+			'url' => [
 				'target' => 'aimeos_shop_jsonadm_get',
-				'config' => array(
+				'config' => [
 					'absoluteUri' => true,
-				),
-				'options' => array(
+				],
+				'options' => [
 					'target' => 'aimeos_shop_jsonadm_options',
-					'config' => array(
+					'config' => [
 						'absoluteUri' => true,
-					),
-				),
-			),
-		),
-	),
-	'client' => array(
-		'html' => array(
-			'account' => array(
-				'index' => array(
-					'url' => array(
+					],
+				],
+			],
+		],
+	],
+	'client' => [
+		'html' => [
+			'account' => [
+				'index' => [
+					'url' => [
 						'target' => 'aimeos_shop_account',
-					),
-				),
-				'subscription' => array(
-					'url' => array(
+					],
+				],
+				'subscription' => [
+					'url' => [
 						'target' => 'aimeos_shop_account',
-					),
-				),
-				'history' => array(
-					'url' => array(
+					],
+				],
+				'history' => [
+					'url' => [
 						'target' => 'aimeos_shop_account',
-					),
-				),
-				'favorite' => array(
-					'url' => array(
+					],
+				],
+				'favorite' => [
+					'url' => [
 						'target' => 'aimeos_shop_account_favorite',
-					),
-				),
-				'watch' => array(
-					'url' => array(
+					],
+				],
+				'watch' => [
+					'url' => [
 						'target' => 'aimeos_shop_account_watch',
-					),
-				),
-				'download' => array(
-					'url' => array(
+					],
+				],
+				'download' => [
+					'url' => [
 						'target' => 'aimeos_shop_account_download',
-					),
-					'error' => array(
-						'url' => array(
+					],
+					'error' => [
+						'url' => [
 							'target' => 'aimeos_shop_account',
-						),
-					),
-				),
-			),
-			'basket' => array(
-				'standard' => array(
-					'url' => array(
+						],
+					],
+				],
+			],
+			'basket' => [
+				'standard' => [
+					'url' => [
 						'target' => 'aimeos_shop_basket',
-					),
-				),
-			),
-			'catalog' => array(
-				'count' => array(
-					'url' => array(
+					],
+				],
+			],
+			'catalog' => [
+				'count' => [
+					'url' => [
 						'target' => 'aimeos_shop_count',
-					),
-				),
-				'detail' => array(
-					'url' => array(
+					],
+				],
+				'detail' => [
+					'url' => [
 						'target' => 'aimeos_shop_detail',
-					),
-				),
-				'lists' => array(
-					'url' => array(
+					],
+				],
+				'lists' => [
+					'url' => [
 						'target' => 'aimeos_shop_list',
-					),
-				),
-				'session' => array(
-					'pinned' => array(
-						'url' => array(
+					],
+				],
+				'session' => [
+					'pinned' => [
+						'url' => [
 							'target' => 'aimeos_shop_session_pinned',
-						),
-					),
-				),
-				'stock' => array(
-					'url' => array(
+						],
+					],
+				],
+				'stock' => [
+					'url' => [
 						'target' => 'aimeos_shop_stock',
-					),
-				),
-				'suggest' => array(
-					'url' => array(
+					],
+				],
+				'suggest' => [
+					'url' => [
 						'target' => 'aimeos_shop_suggest',
-					),
-				),
-				'tree' => array(
-					'url' => array(
+					],
+				],
+				'tree' => [
+					'url' => [
 						'target' => 'aimeos_shop_tree',
-					),
-				),
-			),
-			'checkout' => array(
-				'confirm' => array(
-					'url' => array(
+					],
+				],
+			],
+			'checkout' => [
+				'confirm' => [
+					'url' => [
 						'target' => 'aimeos_shop_confirm',
-					),
-				),
-				'standard' => array(
-					'url' => array(
+					],
+				],
+				'standard' => [
+					'url' => [
 						'target' => 'aimeos_shop_checkout',
-					),
-					'summary' => array(
-						'option' => array(
-							'terms' => array(
-								'url' => array(
+					],
+					'summary' => [
+						'option' => [
+							'terms' => [
+								'url' => [
 									'target' => 'aimeos_shop_terms',
-								),
-								'cancel' => array(
-									'url' => array(
+								],
+								'cancel' => [
+									'url' => [
 										'target' => 'aimeos_shop_terms',
-									),
-								),
-								'privacy' => array(
-									'url' => array(
+									],
+								],
+								'privacy' => [
+									'url' => [
 										'target' => 'aimeos_shop_privacy',
-									),
-								),
-							),
-						),
-					),
-				),
-				'update' => array(
-					'url' => array(
+									],
+								],
+							],
+						],
+					],
+				],
+				'update' => [
+					'url' => [
 						'target' => 'aimeos_shop_update',
-					),
-				),
-			),
-			'common' => array(
-				'content' => array(
+					],
+				],
+			],
+			'common' => [
+				'content' => [
 					'baseurl' => '/',
-				),
-				'template' => array(
+				],
+				'template' => [
 					'baseurl' => './aimeos/elegance',
-				),
-			),
-		),
-		'jsonapi' => array(
-			'url' => array(
+				],
+			],
+		],
+		'jsonapi' => [
+			'url' => [
 				'target' => 'aimeos_shop_jsonapi_options',
-			),
-		),
-	),
+			],
+		],
+	],
 
 
-	'controller' => array(
-		'common' => array(
-			'media' => array(
-				'standard' => array(
-					'mimeicon' => array(
+	'controller' => [
+		'common' => [
+			'media' => [
+				'standard' => [
+					'mimeicon' => [
 						# Directory where icons for the mime types stored
 						'directory' => 'aimeos/mimeicons',
 						# File extension of mime type icons
 						'extension' => '.png'
-					),
-				),
-			),
-		),
-	),
+					],
+				],
+			],
+		],
+	],
 
-	'mshop' => array(
-		'customer' => array(
-			'manager' => array(
-				'password' => array(
+	'mshop' => [
+		'customer' => [
+			'manager' => [
+				'password' => [
 					'name' => 'Bcrypt',
-				),
-			),
-		),
-		'index' => array(
-			'manager' => array(
-				'attribute' => array(
+				],
+			],
+		],
+		'index' => [
+			'manager' => [
+				'attribute' => [
 					'name' => 'MySQL',
-				),
-				'catalog' => array(
+				],
+				'catalog' => [
 					'name' => 'MySQL',
-				),
-				'price' => array(
+				],
+				'price' => [
 					'name' => 'MySQL',
-				),
-				'supplier' => array(
+				],
+				'supplier' => [
 					'name' => 'MySQL',
-				),
-				'text' => array(
+				],
+				'text' => [
 					'name' => 'MySQL',
-				),
-			),
-		),
-	),
+				],
+			],
+		],
+	],
 
-);
+];
--- src/aimeos-settings.php
+++ src/aimeos-settings.php
@@ -1,27 +1,27 @@
 <?php
 
-return array(
+return [
 
 	// 'apc_enabled' => false,
 	// 'apc_prefix' => 'slim:',
 	// 'uploaddir' => '/',
 
-	'page' => array(
-		// 'account-index' => array( 'account/profile','account/subscription','account/history','account/favorite','account/watch','basket/mini','catalog/session' ),
-		// 'basket-index' => array( 'basket/bulk', 'basket/standard','basket/related' ),
-		// 'catalog-count' => array( 'catalog/count' ),
-		// 'catalog-detail' => array( 'basket/mini','catalog/stage','catalog/detail','catalog/session' ),
-		// 'catalog-list' => array( 'basket/mini','catalog/filter','catalog/lists' ),
-		// 'catalog-stock' => array( 'catalog/stock' ),
-		// 'catalog-suggest' => array( 'catalog/suggest' ),
-		// 'catalog-tree' => array( 'basket/mini','catalog/filter','catalog/stage','catalog/lists' ),
-		// 'checkout-confirm' => array( 'checkout/confirm' ),
-		// 'checkout-index' => array( 'checkout/standard' ),
-		// 'checkout-update' => array( 'checkout/update'),
-	),
+	'page' => [
+		// 'account-index' => [ 'account/profile','account/subscription','account/history','account/favorite','account/watch','basket/mini','catalog/session' ],
+		// 'basket-index' => [ 'basket/bulk', 'basket/standard','basket/related' ],
+		// 'catalog-count' => [ 'catalog/count' ],
+		// 'catalog-detail' => [ 'basket/mini','catalog/stage','catalog/detail','catalog/session' ],
+		// 'catalog-list' => [ 'basket/mini','catalog/filter','catalog/lists' ],
+		// 'catalog-stock' => [ 'catalog/stock' ],
+		// 'catalog-suggest' => [ 'catalog/suggest' ],
+		// 'catalog-tree' => [ 'basket/mini','catalog/filter','catalog/stage','catalog/lists' ],
+		// 'checkout-confirm' => [ 'checkout/confirm' ],
+		// 'checkout-index' => [ 'checkout/standard' ],
+		// 'checkout-update' => [ 'checkout/update'],
+	],
 
 	// route prefixes, e.g. {site}, {locale} and {currency} resp. {site} and {lang} for /admin/*
-	'routes' => array(
+	'routes' => [
 		// 'admin' => '/admin',
 		// 'extadm' => '/admin/{site}/extadm',
 		// 'jqadm' => '/admin/{site}/jqadm',
@@ -30,10 +30,10 @@ return array(
 		// 'account' => '/profile',
 		// 'default' => '/shop',
 		// 'update' => '',
-	),
+	],
 
-	'resource' => array(
-		'db' => array(
+	'resource' => [
+		'db' => [
 			'adapter' => 'mysql',
 			'host' => 'localhost',
 			'port' => '',
@@ -41,51 +41,51 @@ return array(
 			'database' => 'slim',
 			'username' => 'aimeos',
 			'password' => 'aimeos',
-			'stmt' => ["SET SESSION sort_buffer_size=2097144; SET NAMES 'utf8mb4'; SET SESSION sql_mode='ANSI'"],
+			'stmt' => ["SET SESSION sort_buffer_size=2097144; SET NAMES 'utf8mb4'; SET SESSION sql_mode='ANSI'; SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED"],
 			'opt-persistent' => 0,
 			'limit' => 3,
 			'defaultTableOptions' => [
 				'charset' => 'utf8mb4',
 				'collate' => 'utf8mb4_bin',
 			],
-		),
-		'fs' => array(
+		],
+		'fs' => [
 			'adapter' => 'Standard',
 			'basedir' => './',
 			'baseurl' => '/', // change to https://<yourdomain>/
-		),
-	),
+		],
+	],
 
-	'client' => array(
-		'html' => array(
-			'common' => array(
-				'template' => array(
+	'client' => [
+		'html' => [
+			'common' => [
+				'template' => [
 					// 'baseurl' => './aimeos/elegance',
-				),
-			),
-		),
-	),
+				],
+			],
+		],
+	],
 
-	'controller' => array(
-	),
+	'controller' => [
+	],
 
-	'i18n' => array(
-	),
+	'i18n' => [
+	],
 
-	'madmin' => array(
-	),
+	'madmin' => [
+	],
 
-	'mshop' => array(
-	),
+	'mshop' => [
+	],
 
 
-	'command' => array(
-	),
+	'command' => [
+	],
 
-	'backend' => array(
-	),
+	'backend' => [
+	],
 
-	'frontend' => array(
-	),
+	'frontend' => [
+	],
 
-);
+];
